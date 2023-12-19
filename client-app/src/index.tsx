422b483846b478770ace7f2294ef543e5a636fed
vidarMaartensson <vidar.maartensson@gmail.com>
2023-12-19 17:11:34 +0100

    End of section 11

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'react-calendar/dist/Calendar.css';
-import 'react-toastify/dist/ReactToastify.min.css'
+import 'react-toastify/dist/ReactToastify.min.css';
+import 'react-datepicker/dist/react-datepicker.css';
 import reportWebVitals from './reportWebVitals';
 import 'semantic-ui-css/semantic.min.css';
 import { StoreContext, store } from './app/stores/store';
